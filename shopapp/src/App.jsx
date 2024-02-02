af80af68bb8483c0f6f18b451efa1664427e9ea6
Huzaifa <76128718+Huzaifa4472@users.noreply.github.com>
2024-02-02 15:46:54 +0500

    changes

--- shopapp/src/App.jsx
+++ shopapp/src/App.jsx
@@ -1,11 +1,11 @@
-// import Home from "./pages/Home";
-import Product from "./pages/Product";
+import Home from "./pages/Home";
+// import Product from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
       <>
-        <Product />
+        <Home />
       </>
     </div>
   );
